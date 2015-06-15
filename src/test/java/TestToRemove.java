99fa0c6ce75b852ed112d9ce80a551e84875d0bb
aesteve <aesteve@evenium.com>
2015-06-15 18:47:01 +0200

    try with JsonObject ?

--- src/test/java/TestToRemove.java
+++ src/test/java/TestToRemove.java
@@ -1,7 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
-
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.impl.Utils;
 
 import javax.script.Bindings;
@@ -13,22 +11,22 @@ import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 public class TestToRemove {
     public static void main(String... args) throws Exception {
+        System.out.println(System.getProperty("java.version"));
         Vertx vertx = Vertx.vertx();
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine nashorn = mgr.getEngineByName("nashorn");
         ScriptContext ctx = nashorn.getContext();
         Bindings bindings = ctx.getBindings(ScriptContext.ENGINE_SCOPE);
         nashorn.eval(Utils.readFileToString(vertx, "externallibs/lodash.min.js"), bindings);
+        nashorn.eval("load(\"nashorn:mozilla_compat.js\");", bindings);
         String tpl = Utils.readFileToString(vertx, "hello.tpl");
         bindings.put("vertxTemplate", tpl);
         nashorn.eval("compiled = _.template(vertxTemplate);", bindings);
         Object compiled = nashorn.get("compiled");
         System.out.println("compiled = " + compiled.getClass());
-        Map<String, String> props = new HashMap<String, String>();
-        props.put("name", "world");
-        bindings.put("props", props);
-        Object propsJs = nashorn.eval("var MapType = Java.type('java.util.HashMap');new MapType(props);", bindings);
-        System.out.println(propsJs.getClass());
-        System.out.println(((ScriptObjectMirror) compiled).call(compiled, propsJs));
+        JsonObject json = new JsonObject();
+        json.put("name", "world");
+        bindings.put("props", json);
+        System.out.println(((ScriptObjectMirror) compiled).call(compiled, bindings.get("props")));
     }
 }
